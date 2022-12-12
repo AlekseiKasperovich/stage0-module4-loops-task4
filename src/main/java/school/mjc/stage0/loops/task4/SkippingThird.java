3daa1ddbaa585ff8f9dbb30c939dfe4f144af1f8
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-12 16:55:58 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
-    public void printUntilButThird(int lastPrinted) {
 
+    public void printUntilButThird(int lastPrinted) {
+        int count = 1;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if (count == 3) {
+                count = 1;
+                continue;
+            }
+            count++;
+            System.out.println(i);
+        }
     }
 }
