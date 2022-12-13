02a9d3d6bf23732b5c030f7cd3693e5a5b2e1d5b
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-13 12:58:15 +0300

    commit3

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -5,15 +5,14 @@ public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
         if (lastInRow < 0) {
             System.out.println("last number in row is negative");
-            System.exit(0);
-        }
-        if (numberToSkip > lastInRow) {
-            System.out.println("number to skip is bugger then the last");
-            System.exit(0);
         }
         int sum1 = 0;
         int sum2 = 0;
         for (int i = 0; i <= lastInRow; i++) {
+            if (numberToSkip > lastInRow) {
+                System.out.println("number to skip is bugger then the last");
+                break;
+            }
             sum1 = sum1 + i;
             if (i == numberToSkip) {
                 sum1 = sum1 - i;
