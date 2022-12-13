3ce608710ed7cbff4f113208416bced755bdfab4
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-13 12:52:42 +0300

    commit2

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
+    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        for (int i = 0; i <= numberToGoUntil; i++) {
+            if (numberToGoUntil < toBreakWith) {
+                if (i == 0) {
+                    System.out.println("iterating till the end");
+                }
+                System.out.println(i);
+            } else {
+                System.out.println(i);
+                if (i == toBreakWith) {
+                    break;
+                }
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,30 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+            System.exit(0);
+        }
+        if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+            System.exit(0);
+        }
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i <= lastInRow; i++) {
+            sum1 = sum1 + i;
+            if (i == numberToSkip) {
+                sum1 = sum1 - i;
+                System.out.println("skipped sum is number " + sum1);
+            }
+            if (i > numberToSkip) {
+                sum2 = sum2 + i;
+            }
+            if (i == lastInRow) {
+                System.out.println("counted sum is number " + sum2);
+            }
+        }
     }
 }
